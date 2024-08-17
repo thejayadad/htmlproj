e937c989013581e0d1ab64923c14b505d5e0952e
thejayadad <74875595+thejayadad@users.noreply.github.com>
2024-08-17 16:51:45 -0400

    array dropdown

---
+++ arraydropdown/main.js
@@ -0,0 +1,9 @@
+const optionsArray = ["Jace", "Jazz", "Jada", "Jax"];
+
+const dropdown = document.getElementById('item');
+
+optionsArray.forEach(optionText => {
+    const optionElement = document.createElement('option');
+    optionElement.textContent = optionText;
+    dropdown.appendChild(optionElement);
+});
