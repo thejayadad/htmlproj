239960e3e17ed90ff549c040ab2b938a5c445800
thejayadad <74875595+thejayadad@users.noreply.github.com>
2024-08-18 07:45:46 -0400

    sort

--- sort-filter/main.js
+++ sort-filter/main.js
@@ -1,10 +1,8 @@
 document.getElementById('btn').addEventListener('click', function() {
     const input = document.getElementById('numberInput').value;
-    const numbersArray = input.split(',').map(Number);
-
-    const oddNumbers = numbersArray.filter(num => num % 2 !== 0);
-    oddNumbers.sort((a, b) => a - b);
-
-    const resultDiv = document.getElementById('result');
-    resultDiv.innerHTML = `<p>Filtered and Sorted Odd Numbers: ${oddNumbers.join(', ')}</p>`;
-});
+    const numbersArray = input.split(',').map(Number)
+    const oddnumbers = numbersArray.filter(num => num %2 !==0)
+    oddnumbers.sort((a,b) => a - b)
+    const resultDiv = document.getElementById('result')
+    resultDiv.innerHTML = `<p>filtered and Sorted Odd Numbers: ${oddnumbers.join(', ')}</p>` 
+})
